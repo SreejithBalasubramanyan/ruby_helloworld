a328f313fcf35ebb012cc89a0fe5a7694c62526f
SREEJITH-P-B <33229709+SREEJITH-P-B@users.noreply.github.com>
2020-11-10 01:57:04 +0000

    completed the calculator task successfully

---
+++ class_instances.rb
@@ -0,0 +1,16 @@
+#!/user/bin/env ruby 
+
+require_relative  'classes/calculator'
+
+#begin
+calculator=Calculator.new(5)
+calculator.add(4)
+#calculator.sub()
+#calculator.mul()
+#calculator.div(2)
+calculator.result
+#rescue ArgumentError
+ #   puts "Missing Argment"
+
+#end 
+
--- classes/calculator.rb
+++ classes/calculator.rb
@@ -1,53 +1,118 @@
 class Calculator
+
     def initialize(arg1=nil)
         if(arg1==nil)
+            @result="Undefined"
             raise ArgumentError
+        elsif (arg1.class!=Integer)
+            @result="Undefined"
+            raise TypeError
+        else 
+            @result=arg1
         end
-        @result=arg1
+       
         rescue ArgumentError
-            puts "Argument is missing "
+            puts "Argument 1 is missing "
+        rescue TypeError
+            puts "Expecting an integer:)"
     end
 
-    def add(operand2)
-        @result=@result+operand2
-        @result
-    end 
-    def sub(operand2)
-        @result=@result-operand2
-        @result
-    end 
-    def mul(operand2)
-        @result=@result*operand2
-        @result
-    end 
-    def div(operand2)
-    	begin
-    		
-        @result=@result/operand2
+
+
+    def add(operand2=nil)
+        if(operand2==nil)
+            @result="Undefined"
+            raise ArgumentError
+        elsif (operand2.class!=Integer)
+            @result="Undefined"
+            raise TypeError
+        else 
+            if(@result!="Undefined")
+                @result=@result+operand2
+                @result
+            end
+        end
+        rescue ArgumentError
+            puts "Argument 2 is missing "
+        rescue TypeError
+            puts "Expecting an integer:)"
+        end 
+
+
+
+
+    def sub(operand2=nil)
+        if(operand2==nil)
+            @result="Undefined"
+            raise ArgumentError
+        elsif (operand2.class!=Integer)
+            @result="Undefined"
+            raise TypeError
+        else 
+            if(@result!="Undefined")
+                @result=@result-operand2
+                @result
+            end
+        end
+
+        rescue ArgumentError
+            puts "Argument 2 is missing "
+        rescue TypeError
+            puts "Expecting an integer:)"
+        
+    end
+    
+    
+    def mul(operand2=nil)
+        if(operand2==nil)
+            @result="Undefined"
+            raise ArgumentError
+        elsif (operand2.class!=Integer)
+            @result="Undefined"
+            raise TypeError
+        else 
+            if(@result!="Undefined")
+                @result=@result*operand2
+                @result
+            end
+        end
+
+        rescue ArgumentError
+            puts "Argument 2 is missing "
+        rescue TypeError
+            puts "Expecting an integer:)"
+        
+    end
+
+
+    def div(operand2=nil)
+        if(operand2==nil)
+            @result="Undefined"
+            raise ArgumentError
+        elsif (operand2.class!=Integer)
+            @result="Undefined"
+            raise TypeError
+        else 
+            if(@result!="Undefined")
+                @result=@result/operand2
+                @result
+            end
+        end
+
+        rescue ArgumentError
+            puts "Argument 2 is missing "
+        rescue TypeError
+            puts "Expecting an integer:)"
         rescue ZeroDivisionError
-    	# Handle the error.
-    	@result='undefined'
-    	puts "ZeroDivisionError (divided by 0) :Operand can't be zero"
-    	end
-    	@result
-    end 
+            # Handle the error.
+            @result='undefined'
+            puts "ZeroDivisionError (divided by 0) :Operand can't be zero"
+     end
+    
     
     def result
     	
-        puts @result
+        puts "result: #{@result}"
      end
 end
-
-    
-#begin
-calculator=Calculator.new()
-#calculator.add(4)
-#calculator.sub(3)
-#calculator.mul(3)
-#calculator.div(2)
-#calculator.result
-#rescue ArgumentError
- #   puts "Missing Argment"
-
-#end 
 
